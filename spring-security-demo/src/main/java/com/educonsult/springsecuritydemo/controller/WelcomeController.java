5b5665ac3fe3ce2248d5e74af89bc643c71cfc7b
Satya Prakash Patel <satyaprakashpatel61@gmail.com>
2024-11-10 22:18:20 +0530

    first commit

---
+++ spring-security-demo/src/main/java/com/educonsult/springsecuritydemo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.educonsult.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Spring Security Demo";
+    }
+}
